111bdd789e2a6ed876a98a72133dd56d4b2ce66a
marcfusch <53045672+marcfusch@users.noreply.github.com>
2023-02-01 14:24:05 +0100

    better randomization

--- mac.sh
+++ mac.sh
@@ -5,8 +5,8 @@ sudo /System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -z
 interface=$(networksetup -listallhardwareports | awk '/Wi-Fi/{getline; print $2}')
 
 hexchars="0123456789abcdef"
-end=$( for i in {1..4} ; do echo -n ${hexchars:$(( $RANDOM % 16 )):1} ; done | sed -e 's/\(..\)/:\1/g' )
-macadd="40:a1:08:df"$end
+end=$( for i in {1..6} ; do echo -n ${hexchars:$(( $RANDOM % 16 )):1} ; done | sed -e 's/\(..\)/:\1/g' )
+macadd="40:a1:08"$end
 
 sudo ifconfig $interface ether $macadd
 sudo networksetup -detectnewhardware
