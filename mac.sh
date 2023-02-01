72335e592acfecc864d1cc3c5a65351e6577eae2
marcfusch <53045672+marcfusch@users.noreply.github.com>
2023-02-01 14:14:32 +0100

    Add files via upload

---
+++ mac.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo /System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -z
+
+interface=$(networksetup -listallhardwareports | awk '/Wi-Fi/{getline; print $2}')
+
+hexchars="0123456789abcdef"
+end=$( for i in {1..4} ; do echo -n ${hexchars:$(( $RANDOM % 16 )):1} ; done | sed -e 's/\(..\)/:\1/g' )
+macadd="40:a1:08:df"$end
+
+sudo ifconfig $interface ether $macadd
+sudo networksetup -detectnewhardware
+
+echo "Your new mac address is:" $macadd
